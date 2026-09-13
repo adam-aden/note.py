5dd7b14268e73e3f824cf739879c72be49f10517
adam-aden <swapadam94@gmail.com>
2026-09-13 14:02:53 +0700

    add btn

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
+#build gui
 import tkinter
 tk = tkinter
 root = tk.Tk()
-
-read = tk.Label(root, text="hi welcome to note", width=100, height=5)
+#build widget
+read = tk.Label(root, text="welcome to note", width=100, height=5)
 read.pack()
 
 write = tk.Entry(root, width=100,)
 write.pack()
 
+show_list = tk.Listbox(root, width=100, height=20)
+show_list.place(x = 380, y = 150)
+
+old_file = open("note.txt", "r")
+content = old_file.read()
+lines = content.split("\n")
+for line in lines:
+    show_list.insert("end", line)
+old_file.close()
+print(content)
+
 ls = []
+
 def show():
     text = write.get()
     print(text)
     ls.append(text)
     print(ls)
-click = tk.Button(root, text= "click", width=40, command=show)
-click.pack()
+    print(content)
+    f = open("note.txt", "a")
+    f.write(text + "\n")
+    f.close()
+    show_list.insert("end", text)
 
+def delete_note():
+    selected = show_list.curselection()
+    print(selected)
+click = tk.Button(root, text= "click", width=10, command=show)
+click.place(x = 902, y = 100)
 
+delete_btn = tk.Button(root, text = "delete", width = 10, command=delete_note)
+delete_btn.place(x = 902, y = 125)
 
     
 root.mainloop()
